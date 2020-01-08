0c60ee636d9ed4cdb6e8375c8775f40a6d37397d
Alvin Muala <Alvin.Muala@sanlam.co.za>
2020-01-08 16:09:49 +0200

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,68 @@
+﻿using AspNetCoreToDo.Models;
+using AspNetCoreToDo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace AspNetCoreToDo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem(TodoItem newItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var successful = await _todoItemService.AddItemAsync(newItem);
+
+            if (!successful)
+            {
+                return BadRequest("Could not add item.");
+            };
+
+            return RedirectToAction("Index");
+        }
+
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MarkDone(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var successful = await _todoItemService.MarkDoneAsync(id);
+
+            if (!successful) 
+            {
+                return BadRequest("Could not make item as done");
+            }
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Services/TodoItemService.cs
@@ -0,0 +1,58 @@
+﻿using AspNetCoreToDo.Data;
+using AspNetCoreToDo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreToDo.Services
+{
+    public class TodoItemService : ITodoItemService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var items = await _context.Items
+                .Where(x => x.IsDone == false)
+                .ToArrayAsync();
+            
+            return items;
+        }
+
+        public async Task<bool> AddItemAsync(TodoItem newItem)
+        {
+            newItem.Id = Guid.NewGuid();
+            newItem.IsDone = false;
+            newItem.DueAt = DateTimeOffset.Now.AddDays(3);
+
+            _context.Items.Add(newItem);
+
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1;
+        }
+
+        public async Task<bool> MarkDoneAsync(Guid id)
+        {
+            var item = await _context.Items
+                .SingleOrDefaultAsync(x => x.Id == id);
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            item.IsDone = true;
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1;
+
+            // One entity should have been updated 
+        }
+    }
+}
